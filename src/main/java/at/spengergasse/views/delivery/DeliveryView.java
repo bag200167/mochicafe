85e8f70639a1ee3618c05d7f81475ce679c6b16d
Jerome Bagui <bag200167@spengergasse.at>
2026-06-08 12:38:02 +0200

    Add Home and Delivery content

--- src/main/java/at/spengergasse/views/delivery/DeliveryView.java
+++ src/main/java/at/spengergasse/views/delivery/DeliveryView.java
@@ -1,5 +1,6 @@
 package at.spengergasse.views.delivery;
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -17,15 +18,42 @@ public class DeliveryView extends VerticalLayout {
 
     public DeliveryView() {
         setSpacing(false);
+        H1 companyName = new H1("Mochi Cafe");
+        companyName.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H2 subName = new H2("... the only coffee you need ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H2 title = new H2("Lieferung");
+
+        H2 zone1 = new H2("5. Bezirk");
+        Paragraph price1 = new Paragraph("Lieferkosten: 3,90 Euro");
+        Paragraph free1 = new Paragraph("Gratislieferung ab 15,00 Euro");
+
+        H2 zone2 = new H2("3. und 4. Bezirk");
+        Paragraph price2 = new Paragraph("Lieferkosten: 5,90 Euro");
+        Paragraph free2 = new Paragraph("Gratislieferung ab 20,00 Euro");
+
+        H2 zone3 = new H2("Wien");
+        Paragraph price3 = new Paragraph("Lieferkosten: 7,90 Euro");
+        Paragraph free3 = new Paragraph("Gratislieferung ab 30,00 Euro");
+
+        H2 zone4 = new H2("Außerhalb von Wien");
+        Paragraph price4 = new Paragraph("Lieferkosten: 20,00 Euro");
+        Paragraph free4 = new Paragraph("Keine Gratislieferung");
+
+        add(
+                companyName, subName, title,
+                zone1, price1, free1,
+                zone2, price2, free2,
+                zone3, price3, free3,
+                zone4, price4, free4
+        );
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
--- src/main/java/at/spengergasse/views/home/HomeView.java
+++ src/main/java/at/spengergasse/views/home/HomeView.java
@@ -1,8 +1,6 @@
 package at.spengergasse.views.home;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -16,16 +14,35 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 public class HomeView extends VerticalLayout {
 
     public HomeView() {
-        setSpacing(false);
+        setSpacing(true);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H1 companyName = new H1("Mochi Cafe");
+        companyName.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H2 subName = new H2("... the only coffee you need...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        Image img = new Image("images/logo.png", "Mochi Cafe Logo");
+        img.setWidth("220px");
+
+        Paragraph description = new Paragraph(
+                "In unserem Mochi Café verbinden wir aromatischen Kaffee aus frisch gerösteten Kaffeebohnen mit den süßen Spezialitäten der philippinischen Dessertkultur. Jede Tasse wird mit Sorgfalt zubereitet, um den vollen Geschmack unserer hochwertigen Bohnen zu entfalten. Von traditionellen Leckereien bis hin zu modernen Mochi-Kreationen – bei uns stehen Qualität, Genuss und ein einzigartiges Café-Erlebnis im Mittelpunkt.\n");
+        description.setWidth("500px");
+        description.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "middle");
+
+        H3 name = new H3("Mochi Cafe GmbH");
+        H3 street = new H3("Spengergasse 20");
+        H3 city = new H3("1050 Wien");
+
+        add(companyName, subName, img, description, name, street, city);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
