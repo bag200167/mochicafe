83fca43a4b473a97d3531133aa38c7df161d8c15
Jerome Bagui <bag200167@spengergasse.at>
2026-06-11 14:46:39 +0200

    Manual Grid and new Icon Implemented

--- src/main/java/at/spengergasse/views/orders/OrdersView.java
+++ src/main/java/at/spengergasse/views/orders/OrdersView.java
@@ -4,10 +4,12 @@ import at.spengergasse.domain.CoffeeOrderException;
 import at.spengergasse.domain.Order;
 import at.spengergasse.service.OrderService;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -30,7 +32,7 @@ public class OrdersView extends VerticalLayout {
     private final Button buttonAddWrong = new Button("Add WRONG order");
 
 
-    private final Grid<Order> grid = new Grid<>(Order.class, true);
+    private final Grid<Order> grid = new Grid<>(Order.class, false);
     private final OrderService coffeeOrderService;
 
     public OrdersView(@Autowired OrderService coffeeOrderService) {
@@ -42,6 +44,34 @@ public class OrdersView extends VerticalLayout {
         buttonAdd10Orders.addClickListener(b -> add10Orders());
         HorizontalLayout buttons = new HorizontalLayout(buttonRemoveAllOrders,buttonAdd10Orders,buttonAddWrong);
         buttons.setSpacing(true);
+
+        grid.addColumn(o -> o.getOrderId())
+                .setHeader("Order ID")
+                .setSortable(true);
+        grid.addColumn(o -> o.getOrderDate())
+                .setHeader("Order Date")
+                .setSortable(true);
+        grid.addColumn(o -> o.getCoffee())
+                .setHeader("Coffee")
+                .setSortable(true);
+        Image p = new Image("icons/coffee.png", "Coffee");
+        p.setWidth("64px");
+        grid.addColumn(o -> o.getSize())
+                .setHeader(new HorizontalLayout(p, new Span("Size")))
+                .setSortable(true);
+        grid.addColumn(o -> o.getQuantity())
+                .setHeader("Quantity")
+                .setSortable(true);
+
+        grid.addComponentColumn(o -> {
+                    Checkbox cb = new Checkbox(o.getSirup());
+                    cb.setReadOnly(true);
+                    return cb;
+                })
+                .setHeader("Syrup")
+                .setSortable(true)
+                .setComparator(o -> o.getSirup());
+
         setSizeFull();
         grid.setSizeFull();
         add(buttons);
